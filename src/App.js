46e0f287ffbcbe8e25ecbd7c7dd01872cf3b50f3
caleb <griffith.caleb@gmail.com>
2020-09-11 11:42:29 -0400

    remove unused dec

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/Header'
 import { About } from './components/About'
-import { SocialMedia } from './components/SocialMedia'
 
 function App() {
   return (
